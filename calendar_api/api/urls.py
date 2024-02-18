244f010e164a74696f53be542a73414ed4b0f804
Nicat-Axundzada <nicataxundzada@gmail.com>
2024-02-18 12:56:33 +0400

    views.py is done

--- calendar_api/api/urls.py
+++ calendar_api/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from api.views import *
+from api.views import EventView, EventDetailView
 
 urlpatterns = [
-    path(''),
+    path("event/", EventView.as_view(), name="event_list"),
+    path("event/<int:pk>", EventDetailView.as_view(), name="event_detail"),
 ]
--- calendar_api/api/views.py
+++ calendar_api/api/views.py
@@ -1,4 +1,53 @@
-from django.shortcuts import render
 from api.models import Event
 from api.serializers import EventSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
+
+
+class EventView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        guest_ids = request.data.get('guest_ids', [])
+        events = Event.objects.filter(guests__id__in=guest_ids).distinct()
+        serializer = EventSerializer(events, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = EventSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class EventDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self, pk):
+        try:
+            return Event.objects.get(pk=pk)
+        except Event.DoesNotExist:
+            raise status.HTTP_404_NOT_FOUND
+
+    def get(self, request, pk):
+        event = self.get_object(pk)
+        serializer = EventSerializer(event)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        event = self.get_object(pk)
+        serializer = EventSerializer(event, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        event = self.get_object(pk)
+        event.is_active = False
+        event.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
